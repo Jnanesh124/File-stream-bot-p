28f343ad49dd394566e3af18ff1f49e288186952
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-10-18 19:15:42 +0600

    Update info.py

--- info.py
+++ info.py
@@ -3,7 +3,7 @@ from os import environ
 
 id_pattern = re.compile(r'^.\d+$')
 
-AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002163804828').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
